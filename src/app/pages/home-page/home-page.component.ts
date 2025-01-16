912f5cf260c7676f76c6272247521958f882c2ea
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-01-17 02:52:04 +0300

    fiyatlandirma baslandi

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroComponentComponent } from './hero-component/hero-component.component';
 import { VerilenHizmetlerComponent } from './verilen-hizmetler/verilen-hizmetler.component';
+import { FiyatlandirmaComponent } from './fiyatlandirma/fiyatlandirma.component';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
-  imports:[HeroComponentComponent, VerilenHizmetlerComponent]
+  imports:[HeroComponentComponent, VerilenHizmetlerComponent, FiyatlandirmaComponent]
 })
 export class HomePageComponent {
 }
