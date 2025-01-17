d72704d9c88b5ac2e992f58e5df6d13d5e80e272
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-01-17 03:31:03 +0300

    global bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { NavbarBootstrapComponent } from './common/navbar-bootstrap/navbar-bootstrap.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarBootstrapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
