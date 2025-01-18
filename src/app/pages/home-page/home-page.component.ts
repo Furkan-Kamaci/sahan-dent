7f02f440e73d3176d6ee9101a66c7f1c47b84153
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-01-18 23:50:04 +0300

    phone button wiggle

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -4,12 +4,14 @@ import { VerilenHizmetlerComponent } from './verilen-hizmetler/verilen-hizmetler
 import { FiyatlandirmaComponent } from './fiyatlandirma/fiyatlandirma.component';
 import { DisHijyenBeltComponent } from './dis-hijyen-belt/dis-hijyen-belt.component';
 import { PhoneButtonComponent } from './phone-button/phone-button.component';
+import { WhatsappButtonComponent } from './whatsapp-button/whatsapp-button.component';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
-  imports:[HeroComponentComponent, VerilenHizmetlerComponent, FiyatlandirmaComponent, DisHijyenBeltComponent, PhoneButtonComponent]
+  imports:[HeroComponentComponent, VerilenHizmetlerComponent, 
+    FiyatlandirmaComponent, DisHijyenBeltComponent, PhoneButtonComponent, WhatsappButtonComponent]
 })
 export class HomePageComponent {
 }
