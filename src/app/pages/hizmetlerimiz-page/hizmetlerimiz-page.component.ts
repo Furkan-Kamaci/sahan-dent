d7e412f34f61b02d1bb3210690beb3cbd1490374
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-01-24 22:26:59 +0300

    hizmetlerimiz page eklendi

---
+++ src/app/pages/hizmetlerimiz-page/hizmetlerimiz-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hizmetlerimiz-page',
+  imports: [],
+  templateUrl: './hizmetlerimiz-page.component.html',
+  styleUrl: './hizmetlerimiz-page.component.scss'
+})
+export class HizmetlerimizPageComponent {
+
+}
