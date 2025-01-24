e33af1549f34f85ec187a8a27e4e0bf07c5685da
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-01-24 22:43:16 +0300

    hizmetlerimiz

--- src/app/pages/hizmetlerimiz-page/hizmetlerimiz-page.component.ts
+++ src/app/pages/hizmetlerimiz-page/hizmetlerimiz-page.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Card {
+  imageUrl: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-hizmetlerimiz-page',
-  imports: [],
   templateUrl: './hizmetlerimiz-page.component.html',
-  styleUrl: './hizmetlerimiz-page.component.scss'
+  styleUrls: ['./hizmetlerimiz-page.component.scss'],
+  imports:[CommonModule]
 })
 export class HizmetlerimizPageComponent {
-
+  cards: Card[] = [
+    { imageUrl: '/images/dis-hijyen-belt.jpeg', description: 'Hijyen' },
+    { imageUrl: '/images/dis-hijyen-belt.jpeg', description: 'Tedavi' },
+    { imageUrl: '/images/dis-hijyen-belt.jpeg', description: 'Bakım' },
+    { imageUrl: '/images/dis-hijyen-belt.jpeg', description: 'Estetik' },
+    { imageUrl: '/images/dis-hijyen-belt.jpeg', description: 'Kontrol' },
+  ];
 }
