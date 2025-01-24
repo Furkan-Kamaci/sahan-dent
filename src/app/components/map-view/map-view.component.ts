21913551464c2537c49d2259199ef1407fe0f3b6
Furkan KAMACI <furkan.kamaci@amadeus.com>
2025-01-24 22:04:38 +0300

    map done

--- src/app/components/map-view/map-view.component.ts
+++ src/app/components/map-view/map-view.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
@@ -9,6 +9,8 @@ import { GoogleMapsModule } from '@angular/google-maps';
   styleUrls: ['./map-view.component.scss']
 })
 export class MapViewComponent {
+  public mapStyles = { width: '90vw', height: '90vw' }; // Default to mobile
+
   center: google.maps.LatLngLiteral = { lat: 40.0982, lng: 26.3955 };
   // zoom = 15;
   zoom = 15;
@@ -21,4 +23,24 @@ export class MapViewComponent {
     // { position: { lat: 37.7849, lng: -122.4094 }, label: 'B' },
     { position: { lat: 40.0982, lng: 26.3955 } },
   ];
+
+  constructor() {
+    console.log("innerWidth: ", window.innerWidth);
+    this.updateMapStyles(window.innerWidth); // Initial style
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.updateMapStyles(event.target.innerWidth);
+  }
+
+  private updateMapStyles(width: number) {
+    if (width <= 768) {
+      // Mobile styles
+      this.mapStyles = { width: '90vw', height: '90vw' };
+    } else {
+      // Desktop styles
+      this.mapStyles = { width: '80vw', height: '60vh' };
+    }
+  }
 }
